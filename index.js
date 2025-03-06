5c92c5d8c046a12ce087dd7a8cd4266d7c10b71e
Derick Sheldrick <sheldrickderick9@gmail.com>
2025-03-06 15:46:14 +0300

    arithmetic submission

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
-// Write your code here
+// Write your code herelet
+let num1 = 2;
+let num2 = 31;
+
+let multiply = (num1 * num2);
+console.log (multiply)
+
+let random =  Math.floor(Math.random()* 10)+1;
+
+let num3 = 19;
+let num4 = 5;
+
+let mod = num3 % num4;
+
+let max = Math.max(4,6,7,8,20);
